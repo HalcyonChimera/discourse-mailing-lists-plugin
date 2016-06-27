ebe7c57150e8532570006f2f917b6f6ea5b39f52
Joshua Bird <joshua.thomas.bird@gmail.com>
2016-06-27 11:37:19 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,1 +1,6 @@
+# name: OSF Mailing Lists for Discourse
+# about: plugin to implement Mailing lists suitable for use by the OSF.
+# version: 0.1
+# author: me
 
+# register_asset "assets/javascripts/discourse/services/osf_service.js.es6"
